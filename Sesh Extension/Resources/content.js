8c9c317aa246123d43cce9b87dbe37f212afa982
Talal <talal.ahm25@gmail.com>
2025-05-22 20:20:29 -0400

    implemented simple grayscale

--- Sesh Extension/Resources/content.js
+++ Sesh Extension/Resources/content.js
@@ -1,7 +1,37 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
+function ensureGrayScaleTagIsCreated() {
+  let style = document.getElementById("grayscale-style");
+  if (!style) {
+    style = document.createElement("style");
+    style.id = "grayscale-style";
+    style.textContent = `
+      html {
+        transition: filter 0.3s ease;
+        filter: none !important;
+      }`;
+    document.head.appendChild(style);
+  }
+}
+
+browser.storage.local.get("grayscaleEnabled").then(({ grayscaleEnabled }) => {
+  ensureGrayScaleTagIsCreated();
+  const style = document.getElementById("grayscale-style");
+  style.textContent = `
+    html {
+      transition: filter 0.3s ease;
+      filter: ${grayscaleEnabled ? "grayscale(100%)" : "none"} !important;
+    }
+  `;
 });
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+browser.runtime.onMessage.addListener((msg) => {
+  if (msg.action === "toggleGrayscale") {
+    ensureGrayScaleTagIsCreated();
+    const style = document.getElementById("grayscale-style");
+    style.textContent = `
+      html {
+        transition: filter 0.3s ease;
+        filter: ${msg.enable ? "grayscale(100%)" : "none"} !important;
+      }
+    `;
+  }
 });
--- Sesh Extension/Resources/popup.js
+++ Sesh Extension/Resources/popup.js
@@ -1,1 +1,23 @@
-console.log("Hello World!", browser);
+const toggle = document.getElementById("toggle");
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const { grayscaleEnabled } =
+    await browser.storage.local.get("grayscaleEnabled");
+  toggle.checked = grayscaleEnabled ?? false;
+  browser.tabs.query({ active: true, currentWindow: true }).then(([tab]) => {
+    browser.tabs.sendMessage(tab.id, {
+      action: "toggleGrayscale",
+      enable: toggle.checked,
+    });
+  });
+});
+
+toggle.addEventListener("change", async () => {
+  await browser.storage.local.set({ grayscaleEnabled: toggle.checked });
+  browser.tabs.query({ active: true, currentWindow: true }).then(([tab]) => {
+    browser.tabs.sendMessage(tab.id, {
+      action: "toggleGrayscale",
+      enable: toggle.checked,
+    });
+  });
+});
